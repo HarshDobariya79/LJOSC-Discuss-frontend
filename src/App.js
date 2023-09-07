0a57a6491dfda3e48e49eb379b24c30df0b0eab6
Harsh Dobariya <harshdobariya79@gmail.com>
2023-09-07 17:58:47 +0530

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return <div className="text-oceanic-blue">LJOSC Home page</div>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);
