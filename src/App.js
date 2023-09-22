4c74916ec1bb7e24476d92838bec430b829ac368
Harsh Dobariya <harshdobariya79@gmail.com>
2023-09-22 15:52:35 +0530

    Add login route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
+import { AuthProvider, useAuth } from "./hooks/useAuth";
+import { ProfileProvider } from "./hooks/useProfile";
+import Login from "./pages/Login/Login";
 
 function App() {
-  return <div className="text-oceanic-blue">LJOSC Home page</div>;
+  const { isLoggedIn } = useAuth();
+
+  return (
+    <div className="bg-[#fffefe] max-w-screen">
+      <Routes>
+        <Route
+          path="/login"
+          element={!isLoggedIn ? <Login /> : <Navigate to="/" />}
+        />
+        <Route path="/signup" element={<Navigate to="/login" />} />
+        <Route path="*" element={<div>Invalid path</div>} />
+      </Routes>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<App />);
+root.render(
+  <AuthProvider>
+    <ProfileProvider>
+      <Router>
+        <App />
+      </Router>
+    </ProfileProvider>
+  </AuthProvider>,
+);
