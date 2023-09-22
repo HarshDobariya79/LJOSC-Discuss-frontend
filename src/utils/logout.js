4eba3b4be76961eff530b590ef9a32a8c1c4ddc5
Harsh Dobariya <harshdobariya79@gmail.com>
2023-09-22 09:46:53 +0530

    Create logout utility

---
+++ src/utils/logout.js
@@ -0,0 +1,9 @@
+const logout = () => {
+  localStorage.clear();
+  sessionStorage.clear();
+  setTimeout(() => {
+    window.location = "/login";
+  }, 500); // This delay depends on useEffect of useAuth.
+};
+
+export default logout;
